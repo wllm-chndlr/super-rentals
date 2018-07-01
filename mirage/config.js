6b1be87d160b0ec4da7ab52f7b15aa0245821d05
William Chandler <chndlr.wllm@gmail.com>
2018-07-01 13:33:30 -0500

    added city filter to rentals page

--- mirage/config.js
+++ mirage/config.js
@@ -1,9 +1,7 @@
 export default function() {
   this.namespace = '/api';
 
-  this.get('/rentals', function() {
-    return {
-      data: [{
+  let rentals = [{
         type: 'rentals',
         id: 'grand-old-mansion',
         attributes: {
@@ -32,11 +30,20 @@ export default function() {
           title: 'Downtown Charm',
           owner: 'Violet Beauregarde',
           city: 'Portland',
-          category: 'Apartmento',
+          category: 'Apartment',
           bedrooms: 3,
           image: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg'
         }
-      }]
-    };
-  });
-}
+      }];
+
+      this.get('/rentals', function(db, request) {
+        if(request.queryParams.city !== undefined) {
+          let filteredRentals = rentals.filter(function(i) {
+            return i.attributes.city.toLowerCase().indexOf(request.queryParams.city.toLowerCase()) !== -1;
+          });
+          return { data: filteredRentals };
+        } else { 
+          return { data: rentals };
+        }
+      });
+    }
